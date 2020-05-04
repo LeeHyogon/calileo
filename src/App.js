422584c0020ef2ae441748f60aca55e591e8a79a
이효곤 <gon109@naver.com>
2020-05-04 21:10:52 +0900

    firestore edit
    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import * as firebase from "firebase"
 import {auth, googleProvider} from "./fb"
 
 const googleLogin = () => {
@@ -15,12 +15,25 @@ const googleLogin = () => {
     let credential = error.credential;
   })
 }
+const db_prc = () => {
+  var db = firebase.firestore();
+  let docRef = db.collection('users').doc('alovelace');
+
+  //firesbase 프로젝트의 데이터베이스 들어가면 확인가능
+  let setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  });
+}
 
 function App() {
   return (
     <div >
       <p> Calileo </p>
       <button onClick = {()=>googleLogin()}> googleLogin </button>
+      <button onClick = {()=>db_prc()}> firestore data insert </button>
+      
     </div>
   );
 }
