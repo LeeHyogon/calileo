346a29f8b3ff927c691d5294003a5efe7db70c29
이효곤 <gon109@naver.com>
2020-05-15 12:26:23 +0900

    mode change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import cs from './App.module.css';
 import './App.css';
 import * as firebase from "firebase"
 import {auth, googleProvider} from "./fb"
+
+import CalenderBody from './ui_comp/CalenderBody'
+import { Container,Button, Icon, Menu, Input, Dropdown } from 'semantic-ui-react'
 
 const googleLogin = () => {
     auth.signInWithPopup(googleProvider).then(function(result) {
@@ -16,33 +20,6 @@ const googleLogin = () => {
     let credential = error.credential;
   })
 }
-<<<<<<< Updated upstream
-const db_prc = () => {
-  var db = firebase.firestore();
-  let docRef = db.collection('users').doc('alovelace');
-  
-  //firesbase 프로젝트의 데이터베이스 들어가면 확인가능
-  let setAda = docRef.set({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815,
-    anyword:'blabla'
-  });
-}
-
-function App() {
-  return (
-    <div >
-      <p> Calileo </p>
-      <button onClick = {()=>googleLogin()}> googleLogin </button>
-      <button onClick = {()=>db_prc()}> firestore data insert </button>
-      
-    </div>
-  );
-}
-
-export default App;
-=======
 const friendOptions = [
   {
     key: 'Y',
@@ -145,4 +122,3 @@ class App extends Component{
   }
 }
 export default App;
->>>>>>> Stashed changes
