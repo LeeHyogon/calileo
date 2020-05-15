84c8f92256d292d4f7270d4888ebde35f8667ebf
이효곤 <gon109@naver.com>
2020-05-15 12:17:52 +0900

    mode change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as firebase from "firebase"
@@ -8,6 +8,7 @@ const googleLogin = () => {
     auth.signInWithPopup(googleProvider).then(function(result) {
     let token = result.credential.accessToken;
     let user = result.user;
+    console.log(user.emailVerified)
     }).catch(function(error) {
     let errorCode = error.code;
     let errorMessage = error.message;
@@ -15,6 +16,7 @@ const googleLogin = () => {
     let credential = error.credential;
   })
 }
+<<<<<<< Updated upstream
 const db_prc = () => {
   var db = firebase.firestore();
   let docRef = db.collection('users').doc('alovelace');
@@ -40,3 +42,107 @@ function App() {
 }
 
 export default App;
+=======
+const friendOptions = [
+  {
+    key: 'Y',
+    text: 'Y',
+    value: 'Y',
+  },
+  {
+    key: 'M',
+    text: 'M',
+    value: 'M',
+  },
+  {
+    key: 'W',
+    text: 'W',
+    value: 'W',
+  },
+  {
+    key: 'D',
+    text: 'D',
+    value: 'D',
+  },
+]
+
+const Selection = () => (
+  <Dropdown
+    placeholder='Select View'
+    fluid
+    selection
+    options={friendOptions}
+  />
+)
+
+const Topblock = () => (
+  <div>
+  <Container>
+    <Menu secondary>
+      <Menu.Menu position='left'>
+        <Menu.Item>
+          <Button basic>
+            <Button.Content>
+              <Icon name='list ul' />
+            </Button.Content>
+          </Button>
+        </Menu.Item>
+      </Menu.Menu>
+      <Menu.Item>
+      <Button basic>
+        <Button.Content>
+          <Icon name='save outline' />
+        </Button.Content>
+      </Button>
+      </Menu.Item>
+      <Menu.Menu position='center'>
+        <Menu.Item>
+          <Button.Group basic size='big' >
+            <Button icon='chevron left' />
+            <Button content='2020년 5월' />
+            <Button icon='chevron right' />
+          </Button.Group>
+        </Menu.Item>
+      </Menu.Menu>
+      <Menu.Menu position='right'>
+        <Menu.Item>
+          <Selection />
+        </Menu.Item>
+        <Menu.Item>
+          <Input icon='search' placeholder='Search task...' />
+        </Menu.Item>
+        <Menu.Item>
+          <Button onClick = {()=>googleLogin()}>
+            <Button.Content>
+              <Icon name='user' />Sign-in
+            </Button.Content>
+          </Button>
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  </Container>
+  </div>
+)
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      mode:'login',
+    }
+  }
+  render(){
+    return(
+      <div >
+      {/* <h3 className={cs.head}> CALILEO </h3> */}
+      {/* <Topblock /> */}
+      {/* <CalenderBody /> */}
+      <button onClick = {()=>googleLogin()}> googleLogin </button>
+      {/* {this.getContent()} */}
+    </div>
+    );
+
+  }
+}
+export default App;
+>>>>>>> Stashed changes
