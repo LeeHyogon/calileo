edb72cf1151cd701013a15c6e71d8f372da3800b
이효곤 <gon109@naver.com>
2020-05-18 16:11:28 +0900

    git pull master again and copy master's app.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
-<<<<<<< HEAD
-import React,{Component, isValidElement} from 'react';
-import logo from './logo.svg';
-import cs from './App.module.css';
-import * as firebase from "firebase"
-import {auth, googleProvider} from "./fb"
-
-import CalenderBody from './ui_comp/CalenderBody'
-import { Container,Button, Icon, Menu, Input, Dropdown } from 'semantic-ui-react'
-
-const googleLogin = () => {
-    auth.signInWithPopup(googleProvider).then(function(result) {
-    let token = result.credential.accessToken;
-    let user = result.user;
-    console.log(user.emailVerified)
-    }).catch(function(error) {
-    let errorCode = error.code;
-    let errorMessage = error.message;
-    let email = error.email;
-    let credential = error.credential;
-  })
-}
-const friendOptions = [
-  {
-    key: 'Y',
-    text: 'Y',
-    value: 'Y',
-  },
-  {
-    key: 'M',
-    text: 'M',
-    value: 'M',
-  },
-  {
-    key: 'W',
-    text: 'W',
-    value: 'W',
-  },
-  {
-    key: 'D',
-    text: 'D',
-    value: 'D',
-  },
-]
-=======
 import React, { Component } from "react";
 import cs from "./CSS/App.module.css";
 
 import Login from "./pages/Login";
 import Main from "./pages/Main";
->>>>>>> 1bee4f7cd719fe533eed157d9e41952f96362ef3
 
 const Selection = () => (
   <Dropdown
