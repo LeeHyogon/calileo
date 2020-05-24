70980e68c61226814149612c10b65eaed0d5da6f
이효곤 <gon109@naver.com>
2020-05-25 00:24:50 +0900

    diff

--- src/UI/MainCalendarBody.js
+++ src/UI/MainCalendarBody.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import moment from 'moment'
 import _ from "lodash"
-
 import { Grid, Menu, Table, Segment,Checkbox, Icon, Button } from 'semantic-ui-react'
 
 function Datetitle(props){
@@ -20,36 +19,34 @@ class MainCalendarBody extends Component{
   }
 
   render(){
+    const {createNew,pivotDay, eventList ,eventCnt}=this.props; 
     var index=this.props.eventCnt; 
     var startTime=[];
     var endTime=[];
-    var i=0;
     //console.log(this.props);
-     startTime=_.map(this.props.eventList,'startTime');
-     endTime=_.map(this.props.eventList,'endTime');
-    
+     startTime=_.map(eventList,'startTime');
+     endTime=_.map(eventList,'endTime');
     return(
       <Table celled >
-        {Datetitle(this.props.pivotDay)}
+        {Datetitle(pivotDay)}
         <Table.Body>
         {_.map(Array(24), (val, timeIndex)=>(
                   <Table.Row >
                   {_.map(Array(7), (val2, dayIndex) =>{
-                    let timeVal = moment(this.props.pivotDay).add(dayIndex, 'd').add(timeIndex, 'h');
+                    let timeVal = moment(pivotDay).add(dayIndex, 'd').add(timeIndex, 'h');
                     var i=0;
                     var ChkTime=false;
                     while(i<index){
-                      if(timeVal.isBetween(moment(startTime[i] ),moment(endTime[i]))
+                       //let diff=moment.duration(moment(endTime[i],"YYYY-MM-DD").diff(moment(startTime[i],"YYYY-MM-DD"))).asDays();
+                      if(timeVal.isBetween(moment(startTime[i]),moment(endTime[i]))
                       || timeVal.isSame(startTime[i])
                       || timeVal.isSame(endTime[i])
-                      ){
-                        ChkTime=true
-                      }
+                      ){ ChkTime=true}
                       i+=1;
                     }
                     return(
                       <Table.Cell style={{backgroundColor : ChkTime ?'yellow': 'white'}}
-                                    selectable verticalAlign='top' onClick = {()=>this.props.createNew(timeVal)
+                                    selectable verticalAlign='top' onClick = {()=>createNew(timeVal)
                       } >
                         {/* {ChkTime ? <Button>{timeVal.format("HH:mm")}</Button>: timeVal.format("HH:mm")} */}
                         {timeVal.format("HH:mm")}
