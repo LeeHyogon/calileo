6f5eb076fa5c608cdd6b99621268eee973ccf047
jonghyun1215 <62870283+jonghyun1215@users.noreply.github.com>
2020-05-28 20:46:43 +0900

    add portal

--- src/UI/CalendarBody.js
+++ src/UI/CalendarBody.js
@@ -1,19 +1,76 @@
 import React from 'react'
 import MainCalendarBody from './MainCalendarBody'
 import SubCalendarBody from './SubCalendarBody'
-import { Grid } from 'semantic-ui-react'
+import CreateEvent from './CreateEvent'
+import { Grid, Input, Button } from 'semantic-ui-react'
+/*
+class CreateEvent extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate :this.props.createNewTime,
+      // moment(this.props.createNewTime).add(1,'h').format("YYYY.MM.DD MM:SS")
+        endDate :"",
+          eventDetail : ""
+    }
+   }
 
-const CalendarBody = () => (
-  <Grid>
-    <Grid.Column width={1}>
-    </Grid.Column>
-    <Grid.Column width={5}>
-      <SubCalendarBody />
-    </Grid.Column>
-    <Grid.Column width={9}>
-      <MainCalendarBody />
-    </Grid.Column>
-  </Grid>
-)
+makeEvent = e => {
+  this.setState({[e.target.name] : e.target.value})
+
+}
+  render(){
+    return(<div>
+      <Input name = "startDate" placeholder = "StartDate" value = {this.state.startDate}
+      onChange = {e=> this.makeEvent(e)}/>
+      <Input name = "endDate" placeholder = "EndDate" value = {this.state.endDate}   onChange = {e=> this.makeEvent(e)}/>
+      <Input name = "eventDetail" placeholder = "EventDetail" value = {this.state.eventDetail}  onChange = {e=> this.makeEvent(e)}/>
+      <Button onClick = {()=>this.props.enlistNew({eventDetail :this.state.eventDetail, startTime : this.state.startDate, endTime : this.state.endDate})} primary>CONFIRM </Button>
+      </div>)
+  }
+}
+*/
+class CalendarBody extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventCnt: 0,
+      isCreateNew : false,
+      createNewTime : "",
+      eventList : []
+    }
+   }
+
+ createNew = (props)=>{
+   this.setState({isCreateNew : true, createNewTime:props.format("YYYY.MM.DD HH:mm")})
+ }
+
+enlistNew = (props) => {
+  this.state.eventCnt+=1;
+  this.setState({eventCnt : this.state.eventCnt,eventList : _.concat(this.state.eventList, props), isCreateNew : false} )
+}
+  render(){
+    return(
+      <Grid>
+        <Grid.Column width={1}>
+        </Grid.Column>
+        <Grid.Column width={5}>
+          <SubCalendarBody />
+          {_.map(this.state.eventList,(val)=> <p>{`${val.eventDetail} ${val.startTime}~${val.endTime}`} </p>)}
+        </Grid.Column>
+        <Grid.Column width={9}>
+          {this.state.isCreateNew ? <CreateEvent createNewTime= {this.state.createNewTime}
+          enlistNew = {this.enlistNew}/>: null}
+          <MainCalendarBody
+            createNew = {this.createNew}
+             pivotDay={this.props.pivotDay}
+             eventList={this.state.eventList}
+             eventCnt={this.state.eventCnt}
+          />
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
 
 export default CalendarBody
--- src/UI/MainCalendarBody.js
+++ src/UI/MainCalendarBody.js
@@ -1,64 +1,63 @@
 import React from 'react'
 import { Grid, Menu, Table, Segment,Checkbox, Icon, Button } from 'semantic-ui-react'
 
-const MainCalendarBody = () => (
-  <Table celled >
-    <Table.Header>
-      <Table.HeaderCell width={2}>Sun</Table.HeaderCell>
-      <Table.HeaderCell width={2}>Mun</Table.HeaderCell>
-      <Table.HeaderCell width={2}>Tue</Table.HeaderCell>
-      <Table.HeaderCell width={2}>Wed</Table.HeaderCell>
-      <Table.HeaderCell width={2}>Thu</Table.HeaderCell>
-      <Table.HeaderCell width={2}>Fri</Table.HeaderCell>
-      <Table.HeaderCell width={2}>Sat</Table.HeaderCell>
-    </Table.Header>
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-      </Table.Row><Table.Row>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-        <Table.Cell selectable verticalAlign='top'><br /><br /><br /></Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-)
+function Datetitle(props){
+  return <Table.Header>{_.map(Array(7),(val, index)=>
+      <Table.HeaderCell width={2}>{moment(props).add(index, 'd').format("MM/DD(ddd)")}
+      </Table.HeaderCell>)}
+  </Table.Header>
+};
+
+class MainCalendarBody extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    const {createNew,pivotDay, eventList ,eventCnt}=this.props;
+    var index=this.props.eventCnt;
+    var startTime=[];
+    var endTime=[];
+    //console.log(this.props);
+     startTime=_.map(eventList,'startTime');
+     endTime=_.map(eventList,'endTime');
+    return(
+      <Table celled >
+        {Datetitle(pivotDay)}
+        <Table.Body>
+        {_.map(Array(24), (val, timeIndex)=>(
+                  <Table.Row >
+                  {_.map(Array(7), (val2, dayIndex) =>{
+                    let timeVal = moment(pivotDay).add(dayIndex, 'd').add(timeIndex, 'h');
+                    var i=0;
+                    var ChkTime=false;
+                    while(i<index){
+                       //let diff=moment.duration(moment(endTime[i],"YYYY-MM-DD").diff(moment(startTime[i],"YYYY-MM-DD"))).asDays();
+                      if(timeVal.isBetween(moment(startTime[i]),moment(endTime[i]))
+                      || timeVal.isSame(startTime[i])
+                      || timeVal.isSame(endTime[i])
+                      ){ ChkTime=true}
+                      i+=1;
+                    }
+                    return(
+                      <Table.Cell style={{backgroundColor : ChkTime ?'yellow': 'white'}}
+                                    selectable verticalAlign='top' onClick = {()=>createNew(timeVal)
+                      } >
+                        {/* {ChkTime ? <Button>{timeVal.format("HH:mm")}</Button>: timeVal.format("HH:mm")} */}
+                        {timeVal.format("HH:mm")}
+                      </Table.Cell>)
+
+                  })}
+                  </Table.Row>))}
+        </Table.Body>
+        {/* {JSON.stringify(this.props.eventList)} */}
+      </Table>
+
+    );
+  }
+}
 
 export default MainCalendarBody
