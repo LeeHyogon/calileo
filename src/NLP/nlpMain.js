2ce1b67ebb98a82f99a68352cf1193d3e1e01e0b
heech912 <heech912@gmail.com>
2020-06-01 17:58:58 +0900

    nlpstart

---
+++ src/NLP/nlpMain.js
@@ -0,0 +1,62 @@
+const { dockStart } = require('@nlpjs/basic');
+// var fs = require("fs")
+// import { dockStart } from '@nlpjs/basic';
+
+
+async function nlptest() {
+  const dock = await dockStart({ use: ['Basic']});
+  const nlp = dock.get('nlp');
+  nlp.addLanguage('en');
+  // Adds the utterances and intents for the NLP
+  nlp.addDocument('en', 'goodbye for now', 'greetings.bye');
+  nlp.addDocument('en', 'bye bye take care', 'greetings.bye');
+  nlp.addDocument('en', 'okay see you later', 'greetings.bye');
+  nlp.addDocument('en', 'bye for now', 'greetings.bye');
+  nlp.addDocument('en', 'i must go', 'greetings.bye');
+  nlp.addDocument('en', 'hello', 'greetings.hello');
+  nlp.addDocument('en', 'hi', 'greetings.hello');
+  nlp.addDocument('en', 'howdy', 'greetings.hello');
+
+  // Train also the NLG
+  nlp.addAnswer('en', 'greetings.bye', 'Till next time');
+  nlp.addAnswer('en', 'greetings.bye', 'see you soon!');
+  nlp.addAnswer('en', 'greetings.hello', 'Hey there!');
+  nlp.addAnswer('en', 'greetings.hello', 'Greetings!');  await nlp.train();
+  const response = await nlp.process('en', 'I should go now');
+  alert(response);
+}
+
+const somefunct = () => {
+  nlptest()
+}
+
+export default somefunct
+
+// const { NlpManager } = require('node-nlp');
+//
+// const manager = new NlpManager({ languages: ['en'] });
+// // Adds the utterances and intents for the NLP
+// manager.addDocument('en', 'goodbye for now', 'greetings.bye');
+// manager.addDocument('en', 'bye bye take care', 'greetings.bye');
+// manager.addDocument('en', 'okay see you later', 'greetings.bye');
+// manager.addDocument('en', 'bye for now', 'greetings.bye');
+// manager.addDocument('en', 'i must go', 'greetings.bye');
+// manager.addDocument('en', 'hello', 'greetings.hello');
+// manager.addDocument('en', 'hi', 'greetings.hello');
+// manager.addDocument('en', 'howdy', 'greetings.hello');
+//
+// // Train also the NLG
+// manager.addAnswer('en', 'greetings.bye', 'Till next time');
+// manager.addAnswer('en', 'greetings.bye', 'see you soon!');
+// manager.addAnswer('en', 'greetings.hello', 'Hey there!');
+// manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+//
+// // Train and save the model.
+// async function somefunct() {
+//     await manager.train();
+//     manager.save();
+//     const response = await manager.process('en', 'I should go now');
+//     console.log(response);
+// }
+//
+// export default somefunct
--- src/UI/MainCalendarBody.js
+++ src/UI/MainCalendarBody.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react'
-import moment from 'moment'
-import _ from "lodash"
-import { Grid, Menu, Table, Segment,Checkbox, Icon} from 'semantic-ui-react'
-import styled from "styled-components"
+import React, { Component } from "react";
+import moment from "moment";
+import _ from "lodash";
+import { Grid, Menu, Table, Segment, Checkbox, Icon } from "semantic-ui-react";
+import styled from "styled-components";
 
-function Datetitle(props){
-  return <Table.Header>{_.map(Array(7),(val, index)=>
-      <Table.HeaderCell width={2}>{moment(props).add(index, 'd').format("MM/DD(ddd)")}
-      </Table.HeaderCell>)}
-  </Table.Header>
-};
+function Datetitle(props) {
+  return (
+    <Table.Header>
+      {_.map(Array(7), (val, index) => (
+        <Table.HeaderCell width={2}>
+          {moment(props)
+            .add(index, "d")
+            .format("MM/DD(ddd)")}
+        </Table.HeaderCell>
+      ))}
+    </Table.Header>
+  );
+}
 
 const StyledButton = styled.button`
   z-index: 1;
@@ -18,89 +25,104 @@ const StyledButton = styled.button`
   font-size: 1rem;
   line-height: 1.5;
   border: 1px solid lightgray;
-  height: ${(props) => props.height || "10vh"};
+  height: ${props => props.height || "10vh"};
   position: absolute;
-  left: ${(props) => props.left || "10vh"};
-  top: ${(props) => props.top || "10vh"};
-  
-`
-function Button({children,height,left,top}) {
+  left: ${props => props.left || "10vh"};
+  top: ${props => props.top || "10vh"};
+`;
+
+function Button({ children, height, left, top }) {
   return (
     <StyledButton height={height} left={left} top={top}>
       {children}
     </StyledButton>
-  )
+  );
 }
 
-
-class MainCalendarBody extends Component{
+class MainCalendarBody extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    }
+    this.state = {};
   }
-  
-  render(){
-    const {createNew,pivotDay, eventList ,eventCnt}=this.props;
-    var index=this.props.eventCnt;
-    var startTime=[];
-    var endTime=[];
+
+  render() {
+    const { createNew, pivotDay, eventList, eventCnt } = this.props;
+    var index = this.props.eventCnt;
+    var startTime = [];
+    var endTime = [];
     // startTime=_.map(eventList,'startTime');
     // endTime=_.map(eventList,'endTime');
     const mapToComponent = eventList => {
-      var index=eventList.length;
-      var startTime=[];
-      var endTime=[];
-      startTime=_.map(eventList,'startTime');
-      endTime=_.map(eventList,'endTime');
+      var index = eventList.length;
+      var startTime = [];
+      var endTime = [];
+      startTime = _.map(eventList, "startTime");
+      endTime = _.map(eventList, "endTime");
       return eventList.map((person, i) => {
-        let diff=moment.duration(moment(endTime[i],"YYYY-MM-DD").diff(moment(startTime[i],"YYYY-MM-DD"))).asDays();
-        let top=moment(startTime[i]).month()+1;
-        let left=moment(startTime[i]).day(); 
+        let diff = moment
+          .duration(
+            moment(endTime[i], "YYYY-MM-DD").diff(
+              moment(startTime[i], "YYYY-MM-DD")
+            )
+          )
+          .asDays();
+        let top = moment(startTime[i]).month() + 1;
+        let left = moment(startTime[i]).day();
         // let height=moment.duration(moment(endTime[i]).diff(moment(startTime[i]))).asHours();
-        console.log(top+" "+left+" ");
-        return (<Button top={top} left={left} >{top}{left}</Button>);
+        console.log(top + " " + left + " ");
+        return (
+          <Button top={top} left={left}>
+            {top}
+            {left}
+          </Button>
+        );
       });
     };
-    return(
-      <Table celled >
+    return (
+      <div>
+      <Table celled>
         {Datetitle(pivotDay)}
         <Table.Body>
-        {mapToComponent(eventList)}
-        {_.map(Array(24), (val, timeIndex)=>(
-                  <Table.Row >
-                  {_.map(Array(7), (val2, dayIndex) =>{
-                    let timeVal = moment(pivotDay).add(dayIndex, 'd').add(timeIndex, 'h');
-                    // var i=0;
-                    var ChkTime=false;
-                    // while(i<index){
-                    //    //let diff=moment.duration(moment(endTime[i],"YYYY-MM-DD").diff(moment(startTime[i],"YYYY-MM-DD"))).asDays();
-                    //   if(timeVal.isBetween(moment(startTime[i]),moment(endTime[i]))
-                    //   || timeVal.isSame(startTime[i])
-                    //   || timeVal.isSame(endTime[i])
-                    //   ){ ChkTime=true}
-                    //   i+=1;
-                    // }      
-                    return(
-                      <Table.Cell style={{backgroundColor : ChkTime ?'yellow': 'white'}
-                    }
-                                    selectable verticalAlign='top' onClick = {()=>createNew(timeVal)
-                      } >
-                        {/* {ChkTime ? <Button>{timeVal.format("HH:mm")}</Button>: timeVal.format("HH:mm")} */}
-                        {timeVal.format("HH:mm")}
-                      </Table.Cell>)
-
-                  })}
-                  </Table.Row>))}
+          {mapToComponent(eventList)}
+          {_.map(Array(24), (val, timeIndex) => (
+            <Table.Row>
+              {_.map(Array(7), (val2, dayIndex) => {
+                let timeVal = moment(pivotDay)
+                  .add(dayIndex, "d")
+                  .add(timeIndex, "h");
+                // var i=0;
+                var ChkTime = false;
+                // while(i<index){
+                //    //let diff=moment.duration(moment(endTime[i],"YYYY-MM-DD").diff(moment(startTime[i],"YYYY-MM-DD"))).asDays();
+                //   if(timeVal.isBetween(moment(startTime[i]),moment(endTime[i]))
+                //   || timeVal.isSame(startTime[i])
+                //   || timeVal.isSame(endTime[i])
+                //   ){ ChkTime=true}
+                //   i+=1;
+                // }
+                return (
+                  <Table.Cell
+                    style={{ backgroundColor: ChkTime ? "yellow" : "white" }}
+                    selectable
+                    verticalAlign="top"
+                    onClick={() => createNew(timeVal)}
+                  >
+                    {/* {ChkTime ? <Button>{timeVal.format("HH:mm")}</Button>: timeVal.format("HH:mm")} */}
+                    {timeVal.format("HH:mm")}
+                  </Table.Cell>
+                );
+              })}
+            </Table.Row>
+          ))}
         </Table.Body>
         {/* {JSON.stringify(this.props.eventList)} */}
-      
+
         {/* <Button height="1vh">연습용 버튼</Button> */}
       </Table>
-      
+      <button onClick = {()=>this.props.createNewString()}> String으로 일정 만들기 </button>
+      </div>
     );
   }
 }
 
-export default MainCalendarBody
+export default MainCalendarBody;
