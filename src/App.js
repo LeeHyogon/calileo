fb0e9d3edfe574e43eb6978f7ce97bda006582f0
이효곤 <gon109@naver.com>
2020-08-23 12:09:01 +0900

    깃 점검

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Login from "./pages/Login";
 import Main from "./pages/Main";
 
 //깃 변경사항 테스트
-
-//종현님 브랜치 테스트
+//변경 다시체크
 class App extends Component {
   constructor(props) {
     super(props);
